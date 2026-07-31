ec3070e6bbdabb400a8f40ef12ff21817490305f
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-07-31 09:18:41 -0300

    config(scaffold): initial SpringBoot project generated by IntelliJ IDEA

---
+++ src/main/java/com/quistock/ds_backend/DsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quistock.ds_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsBackendApplication.class, args);
+    }
+
+}
