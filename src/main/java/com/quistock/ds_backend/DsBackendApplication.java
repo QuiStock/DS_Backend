7ec959537899a51c333275133e6c22090ca4ca4f
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-07-31 09:28:50 -0300

    chore(linting): configure spotless

--- src/main/java/com/quistock/ds_backend/DsBackendApplication.java
+++ src/main/java/com/quistock/ds_backend/DsBackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DsBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DsBackendApplication.class, args);
-    }
-
+  static void main(String[] args) {
+    SpringApplication.run(DsBackendApplication.class, args);
+  }
 }
