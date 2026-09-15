7d26e67527d6e779d175f0a6da6ba0faf4826e26
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-09-15 08:27:41 -0300

    fix(code smell): fix pre-existing code smells after including analysis in pipeline

--- src/main/java/com/quistock/ds_backend/exception/ErpIntegrationException.java
+++ src/main/java/com/quistock/ds_backend/exception/ErpIntegrationException.java
@@ -1,6 +1,9 @@
 package com.quistock.ds_backend.exception;
 
+import java.io.Serial;
+
 public class ErpIntegrationException extends RuntimeException {
+  @Serial private static final long serialVersionUID = 1233455789;
 
   public ErpIntegrationException(String message, Throwable cause) {
     super(message, cause);
